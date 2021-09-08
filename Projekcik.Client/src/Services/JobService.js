103dadbde52e2638fc72cfa5a34117728d1bd669
Magdalena Majta <majta.magdalena@gmail.com>
2021-09-08 02:29:37 +0200

    job frontend

--- Projekcik.Client/src/Services/JobService.js
+++ Projekcik.Client/src/Services/JobService.js
@@ -3,7 +3,8 @@ import Api from "./Api";
 export default {
   getJobsList: () => Api.get("/job"),
 
-  createJob: (name) => Api.post("/job", { name }),
+  // todo:
+  // createJob: (name) => Api.post("/job", { name }),
 
-  editJob: (id, name) => Api.put(`/job/${id}`, { name }),
+  // editJob: (id, name) => Api.put(`/job/${id}`, { name }),
 };
